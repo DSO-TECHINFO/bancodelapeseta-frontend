d5b752fd935e3ca2a78861f6faa0427d000cf2f7
EzequielJara <ezequierjaraepet20@gmail.com>
2024-02-15 19:22:43 -0300

    Loguin prototipo n1

---
+++ src/app/common-login/common-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './common-login.component.html',
+  styleUrl: './common-login.component.css'
+})
+export class CommonLoginComponent {
+ 
+}
