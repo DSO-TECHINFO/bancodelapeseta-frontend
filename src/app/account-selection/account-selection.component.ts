e553dc73cc337fc69b897b6b9d44b071acaae0a1
rafazf <gerardo1022@hotmail.com>
2024-02-16 17:52:53 -0500

    crearTuCuenta

---
+++ src/app/account-selection/account-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-selection.component.html',
+  styleUrl: './account-selection.component.css'
+})
+export default class AccountSelectionComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import AccountSelectionComponent from './account-selection/account-selection.component';
+import { CommonLoginComponent } from './common-login/common-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',title:'Banco de la Peseta', component:CommonLoginComponent}
+,
+    {path:'account-selection',title:'Seleccion de Cuenta', component:AccountSelectionComponent}
+];
