e6dc8c5ac63fbaf3b747603f812722f3df192b26
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-02-18 19:46:12 +0100

    updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BancodelapesetaFrontend
+# Bancodelapeseta
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'bancodelapeseta-frontend' title`, () => {
+  it(`should have the 'bancodelapeseta' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bancodelapeseta-frontend');
+    expect(app.title).toEqual('bancodelapeseta');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bancodelapeseta-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bancodelapeseta');
   });
 });
