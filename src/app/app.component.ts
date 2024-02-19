279727754c43654beaf761ff24f690b2316c6638
rafazf <gerardo1022@hotmail.com>
2024-02-19 10:51:22 -0500

    authv1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CommonLoginComponent } from './common-login/common-login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CommonLoginComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    CommonLoginComponent,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   
 })
 export class AppComponent {
-  ;
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import AccountSelectionComponent from './account-selection/account-selection.component';
 import { CommonLoginComponent } from './common-login/common-login.component';
 import { PersonalAccountComponent } from './personal-account/personal-account.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
     {path:'login',title:'Banco de la Peseta',component:CommonLoginComponent},
     {path:'',title:'Banco de la Peseta',component:CommonLoginComponent},
     {path:'personal-account',title:'Cuenta Personal',component:PersonalAccountComponent},
-    {path:'account-selection',title:'Seleccion de Cuenta',component:AccountSelectionComponent}
+    {path:'account-selection',title:'Seleccion de Cuenta',component:AccountSelectionComponent},
+    {path:'inicio',title:'Inicio',component:InicioComponent},
 ];
