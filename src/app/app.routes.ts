579e687d29c0806f676361c409259b5be9edcc97
Carlos Ochoa <uligibson@hotmail.com>
2024-02-19 23:01:05 -0600

    feat(account-verification-page): Add layout TODO: implement interactivity and finalize styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AccountLoginComponent } from './pages/profile/account/account-login/account-login.component';
 import { AccountSelectionComponent } from './pages/profile/account/account-selection/account-selection.component';
+import { AccountVerificationComponent } from './pages/profile/account/account-verification/account-verification.component';
 import { RegisterIndividualComponent } from './pages/profile/register/register-individual/register-individual.component';
 import { RegisterCompanyComponent } from './pages/profile/register/register-company/register-company.component';
 
@@ -8,6 +9,7 @@ export const routes: Routes = [
     { path: '', title: 'Banco de la Peseta', component: AccountLoginComponent },
     { path: 'account-login', title: 'Banco de la Peseta', component: AccountLoginComponent },
     { path: 'account-selection', title: 'Seleccion de Cuenta', component: AccountSelectionComponent },
+    { path: 'account-verification', title: 'Verificar cuenta', component: AccountVerificationComponent },
     { path: 'register-company', title: 'Registro cuenta de empresa', component: RegisterCompanyComponent },
     { path: 'register-individual', title: 'Registro cuenta personal', component: RegisterIndividualComponent }
 ];
