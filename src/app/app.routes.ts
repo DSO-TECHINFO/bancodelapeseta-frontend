000bcee2d80d677ee7a282fb8359a07b0cc11a94
Carlos Ochoa <uligibson@hotmail.com>
2024-02-20 22:22:22 -0600

    add: AccountVerificationComponent files (Still in development)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import AccountSelectionComponent from './account-selection/account-selection.component';
+import { AccountVerificationComponent } from './account-verification/account-verification.component';
 import { CommonLoginComponent } from './login/common-login.component';
 import { PersonalAccountComponent } from './personal-account/personal-account.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -9,5 +10,6 @@ export const routes: Routes = [
     {path:'',title:'Banco de la Peseta',component:CommonLoginComponent},
     {path:'personal-account',title:'Cuenta Personal',component:PersonalAccountComponent},
     {path:'account-selection',title:'Seleccion de Cuenta',component:AccountSelectionComponent},
+    {path:'account-verification',title:'Verificación de cuenta',component:AccountVerificationComponent},
     {path:'inicio',title:'Inicio',component:InicioComponent},
 ];
