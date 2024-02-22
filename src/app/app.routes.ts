0213de75e411bef365e1a3619271af374c577b38
Manuel Ángel Rodríguez Segura <marodriguez@hiberus.com>
2024-02-22 16:00:30 +0100

    Fix app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,27 @@ import AccountSelectionComponent from './account-selection/account-selection.com
 import { CommonLoginComponent } from './login/common-login.component';
 import { PersonalAccountComponent } from './personal-account/personal-account.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { AccountVerificationComponent } from './account-verification/account-verification.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoadingComponent } from './loading/loading.component';
 
 export const routes: Routes = [
-    {path:'login',title:'Banco de la Peseta',component:CommonLoginComponent},
-    {path:'',title:'Banco de la Peseta',component:CommonLoginComponent},
-    {path:'personal-account',title:'Cuenta Personal',component:PersonalAccountComponent},
-    {path:'account-selection',title:'Seleccion de Cuenta',component:AccountSelectionComponent},
-    {path:'inicio',title:'Inicio',component:InicioComponent},
-    {path:'dashboard',title:'Inicio',component:DashboardComponent},
+  {
+    path: 'login',
+    title: 'Banco de la Peseta',
+    component: CommonLoginComponent,
+  },
+  { path: '', title: 'Banco de la Peseta', component: CommonLoginComponent },
+  {
+    path: 'personal-account',
+    title: 'Cuenta Personal',
+    component: PersonalAccountComponent,
+  },
+  {
+    path: 'account-selection',
+    title: 'Seleccion de Cuenta',
+    component: AccountSelectionComponent,
+  },
+  { path: 'inicio', title: 'Inicio', component: InicioComponent },
+  { path: 'dashboard', title: 'Inicio', component: DashboardComponent },
+  { path: 'loading', title: 'Loading', component: LoadingComponent },
 ];
-
