5143d1f8528ce8d2c3422e4f292e42ca46e74752
Carlos Ochoa <uligibson@hotmail.com>
2024-02-21 20:28:02 -0600

    tailwindcss implemented. use its classes with "tw" prefix. i. e. tw-bg-white

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  important: true,
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
