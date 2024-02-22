a22c3b1b3f842aab95410a0c393784095d17cf5f
Carlos Ochoa <uligibson@hotmail.com>
2024-02-21 20:49:51 -0600

    using app.routes.ts from dev branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import AccountSelectionComponent from './account-selection/account-selection.component';
-import { AccountVerificationComponent } from './account-verification/account-verification.component';
 import { CommonLoginComponent } from './login/common-login.component';
 import { PersonalAccountComponent } from './personal-account/personal-account.component';
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -12,7 +11,6 @@ export const routes: Routes = [
     {path:'',title:'Banco de la Peseta',component:CommonLoginComponent},
     {path:'personal-account',title:'Cuenta Personal',component:PersonalAccountComponent},
     {path:'account-selection',title:'Seleccion de Cuenta',component:AccountSelectionComponent},
-    {path:'account-verification',title:'Verificación de cuenta',component:AccountVerificationComponent},
     {path:'inicio',title:'Inicio',component:InicioComponent},
     {path:'account-verification',title:'Inicio',component:AccountVerificationComponent},
 ];
