f4f74468b909f6ea9f3f8c74d7da6ff1ab9e9105
rayexbomx <81080877+rayexbomx@users.noreply.github.com>
2024-02-22 17:39:04 +0100

    updated

---
+++ src/app/create-account/create-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-account.component.html',
+  styleUrl: './create-account.component.css'
+})
+export class CreateAccountComponent {
+
+}
