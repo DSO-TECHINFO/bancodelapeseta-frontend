9cbccd836a58920c5ca048420b9b9900fb9ec37d
Carlos Ochoa <uligibson@hotmail.com>
2024-02-22 19:10:23 -0600

    refactor: AccountVerificationComponent (still WIP)

--- src/app/account-verification/account-verification.component.ts
+++ src/app/account-verification/account-verification.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-account-verification',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './account-verification.component.html',
-  styleUrl: './account-verification.component.css'
+  styleUrls: ['./account-verification.component.css'],
 })
 export class AccountVerificationComponent {
-  hide: boolean = false;
-  imgBancoP = './assets/bancoP.png'
+  imgBancoP = './assets/bancoP.png';
+
+  firstOtp = ""
+  secondOtp = ""
+
+  verifyAccount() {
+    console.log("First OTP: " + this.firstOtp + " Second OTP: " + this.secondOtp)
+  }
+
+  // @Input({ required: true })
+  // valueFromParent: string = '';
+
+  // @Output()
+  // updateEvent = new EventEmitter<string>();
+
+  // updateValue() {
+  //   this.updateEvent.emit('Send updated value');
+  // }
 }
