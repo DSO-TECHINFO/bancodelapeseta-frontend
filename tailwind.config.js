29530c91d3f598f981102e42feef811a10f0ac00
Carlos Ochoa <uligibson@hotmail.com>
2024-02-22 19:08:49 -0600

    Fix: bootstrap and tailwind conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: "tw-",
-  important: true,
+  important: false,
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'aqua': '#17DCBF',
+        'vivid-blue': '#425CFF',
+        'sky-blue': '#2E98E0',
+        'light-teal': '#4fd1c5',
+        'hover-teal': '#5fe3d1',
+      },
+    },
   },
   plugins: [],
 };
