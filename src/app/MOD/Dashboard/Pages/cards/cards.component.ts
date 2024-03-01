d0f146e3c635b4e53f2dbb23c6181682b906f839
David Garcia Maria <davidgama260402@gmail.com>
2024-03-01 16:21:21 +0100

    Cards page and card component created, no api connect

--- src/app/MOD/Dashboard/Pages/cards/cards.component.ts
+++ src/app/MOD/Dashboard/Pages/cards/cards.component.ts
@@ -1,9 +1,12 @@
+import { WIndividualCardComponent } from '@/SHARED/Widgets/w-individual-card/w-individual-card.component';
 import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  standalone:true
+  standalone:true,
+  imports:[TranslateModule,WIndividualCardComponent]
 })
 export default class CardsComponent   {
 
---
+++ src/app/SHARED/Widgets/w-individual-card/w-individual-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-w-individual-card',
+  templateUrl: './w-individual-card.component.html',
+  standalone: true
+
+})
+export class WIndividualCardComponent{
+
+  constructor() { }
+
+
+}
