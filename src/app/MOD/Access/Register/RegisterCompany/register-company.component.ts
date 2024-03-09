a4e765939c1237f270cc003c3d35dcf0b514bc58
Carlos Ochoa <uligibson@hotmail.com>
2024-03-08 18:50:22 -0600

    Refactor: register-company.component

--- src/app/MOD/Access/Register/RegisterCompany/register-company.component.ts
+++ src/app/MOD/Access/Register/RegisterCompany/register-company.component.ts
@@ -1,9 +1,13 @@
-import { PasswordValidatorDirective } from '@/SHARED/Directives/password-validator.directive';
+import { WInputComponent } from '@/SHARED/Widgets/input-app';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+// import { PasswordValidatorDirective } from '@/SHARED/Directives/password-validator.directive'; // TODO: REMOVE THIS
+// import { TokenService } from '@/CORE/Auth/services/token-service.service';
+// import { LoginRequest } from '../../Login/interface/loginRequest.interface';
+// import { LoginService } from '../../Login/service/login.service';
 
 @Component({
   selector: 'app-register-company',
@@ -16,7 +20,7 @@ import { IonicModule } from '@ionic/angular';
     FormsModule,
     ReactiveFormsModule,
     CommonModule,
-    PasswordValidatorDirective,
+    WInputComponent,
   ],
   templateUrl: './register-company.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,31 +29,85 @@ export default class RegisterCompanyFormComponent {
   //
   title = 'Company sign up';
 
-  constructor(private router: Router) {}
+  // constructor(private router: Router) {}
 
-  form = {
+  data = {
+    name: "", // "Programa",
+    taxId:"", // "B123456789",
+    password:"", // "%Testing12",
+    address:"", // "Calle hola mi amigo",
+    addressAdditionalInfo:"", // "Piso 2 Escalera 3",
+    postalCode: "", // "22454",
+    addressTown:"", // "Madrid",
+    addressCity:"", // "Madrid",
+    addressCountry:"", // "España",
+    phoneNumber:"", // "665666555",
+    email:"", // "test@test.com",
+    debtType:"", // "PYME",
+    settingUpDate: "", // "2021-07-01"
+  }
+  
+
+  registrationFormValues = {
     companyName: '',
     taxId: '',
-    password: '',
-    confirmPassword: '',
+    addressCountry: '',
+    postalCode: '',
+    addressCity: '',
+    addressTown: '',
     address: '',
     addressAdditionalInfo: '',
-    postalCode: '',
-    addressTown: '',
-    addressCity: '',
-    addressCountry: '',
     phoneNumber: '',
     email: '',
-    debtType: '',
-    settingUpDate: '',
+    password: '',
+    confirmPassword: '',
     acceptTerms: false,
   };
 
-  onSubmit(): void {
-    // console.log(JSON.stringify(this.form));
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    // private loginService: LoginService,
+    // private tokenService: TokenService
+
+  ) {
+    // if(tokenService.getToken()){
+    //   router.navigate(['/dashboard'])
+    // }
+  }
+  // customPattern: string = '^[0-9@]*$'
+
+  registrationForm = this.formBuilder.group({
+    // companyName: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+    // taxId: ['', [Validators.required]],
+  });
+  
+  signup() {
+    console.log("bank");
+    // console.log(this.registrationForm.controls?.companyName);
+    //
+    // if (this.registrationForm.valid) {
+    //   this.loginService
+    //     .login(this.registrationForm.value as LoginRequest, 'api/v1/auth/login')
+    //     .subscribe({
+    //       next: (userData) => {
+    //         this.tokenService.setToken(userData['token']);
+    //       },
+    //       error: (err) => {
+    //         console.error(err);
+    //       },
+    //       complete: () => {
+    //         this.router.navigateByUrl('/dashboard');
+    //         this.registrationForm.reset();
+    //       },
+    //     });
+    // } else {
+    //   this.registrationForm.markAllAsTouched();
+    // }
   }
 
-  onReset(form: NgForm): void {
-    form.resetForm();
-  }
+  // get username(){
+  //   return this.registrationForm.controls.username;
+  // }
+
 }
