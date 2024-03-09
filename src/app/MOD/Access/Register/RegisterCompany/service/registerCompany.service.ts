9bde0c9ab948e5aa826c6dcb36daf8e8f6e97ca1
Carlos Ochoa <uligibson@hotmail.com>
2024-03-08 21:01:11 -0600

    Add RegisterCompanyService to handle company registration

---
+++ src/app/MOD/Access/Register/RegisterCompany/service/registerCompany.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { RegisterCompanyRequest } from '../interface/registerCompanyRequest.interface';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { RegisterCompanyResponse } from '../interface/registerCompanyResponse.interface';
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterCompanyService {
+  constructor(private http: HttpClient) {}
+  urlBase: string = 'https://api.bancodelapeseta.com';
+  // urlBase: string = 'http://localhost:8080';
+
+  signup(data: RegisterCompanyRequest, uri: string): Observable<number> {
+    return this.http
+      .post<RegisterCompanyResponse>(`${this.urlBase}/${uri}`, data, {
+        observe: 'response',
+      })
+      .pipe(
+        map(response => response.status),
+        catchError(this.handleError)
+      );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.log('An error has occurred', error.error);
+    } else {
+      console.log('Backend status code', error.status, error.error);
+    }
+    return throwError(() => new Error('Something failed, try again.'));
+  }
+}
