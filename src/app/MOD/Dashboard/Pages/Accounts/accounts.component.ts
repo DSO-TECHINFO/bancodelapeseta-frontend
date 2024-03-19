eefc22dbc8e3020021681e9552259683db9906a6
rafazf <gerardo1022@hotmail.com>
2024-03-19 16:44:02 -0500

    accounts

--- src/app/MOD/Dashboard/Pages/Accounts/accounts.component.ts
+++ src/app/MOD/Dashboard/Pages/Accounts/accounts.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AccountService } from './service/account.service';
+import { IAccountData } from './interface/IAccount';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
 })
-export default class ClientesComponent implements OnInit{
-  constructor(private readonly accountService: AccountService) {}
-  ngOnInit(): void {
-    this.getAccounts();
-  }
+export default class ClientesComponent implements OnInit {
+  accountData: IAccountData[] = [];
 
-  getAccounts(){
-    this.accountService.getAccounts('api/v1/accounts').subscribe((item)=>{
-      console.log("Datos: ", item);
-    })
+  constructor(private accountService: AccountService) {}
+  ngOnInit() {
+    this.showAccount();
+  }
+  showAccount() {
+    this.accountService.getAccounts('api/v1/accounts').subscribe(
+      {
+        next: (data: IAccountData[]) => {console.log(data);  this.accountData = data},
+      }
+    );
   }
 }
