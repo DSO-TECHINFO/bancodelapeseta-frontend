68e4c92b325fd785f3077bacfb0a5af1636518f4
Flavio Angulo Sumarriva <flavioangulosum@gmail.com>
2024-03-20 14:39:53 -0500

    Ngx-chart

--- src/app/MOD/Dashboard/Pages/inicio/inicio.component.ts
+++ src/app/MOD/Dashboard/Pages/inicio/inicio.component.ts
@@ -4,6 +4,9 @@ import BalanceComponent from './balance/balance.component';
 import CardsComponent from './cards/cards.component';
 import TpvComponent from './tpv/tpv.component';
 import LoansComponent from './loans/loans.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { multi } from './data';
 import { StartPanelService } from './servicio/start-panel.service';
 import { CurrencyExchangeList } from './interface/currencyExchange.interface';
 
@@ -14,7 +17,9 @@ import { CurrencyExchangeList } from './interface/currencyExchange.interface';
     BalanceComponent,
     CardsComponent,
     LoansComponent,
-    TpvComponent
+    TpvComponent,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   templateUrl: './inicio.component.html',
 })
@@ -28,6 +33,41 @@ export default class InicioComponent{
   onRateSelected(rate: number){
     this.currencyExchangeRate = rate;
     console.log("Rate en inicio component: ", rate);
+  }
+
+  multi?: any[];
+  view: any = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor() {
+    Object.assign(this, { multi });
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
