5af9dd72a66057ede24dd06316ef8729ca7969b4
Carlos Ochoa <uligibson@hotmail.com>
2024-03-20 19:04:33 -0600

    add more custom colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,21 @@ module.exports = {
   theme:{
     extend: {
       colors: {
-        primary: '#425cfd',
-        'primary-dark': '#3b53e2',
-        'primary-light': '#c7cbed',
-        'primary-text': '#45435e',
+        primary: {
+          DEFAULT: '#425cfd',
+          dark: '#3b53e2',
+          light: '#c7cbed',
+          text: '#45435e',
+        },
         'background-gray': '#f0f0f7',
         'gray-icons': '#b9b9c7',
         'dark': '#282739',
+        green: {
+          DEFAULT: 'hsl(129 78% 52%)',
+        },
+        yellow: {
+          DEFAULT: 'hsl(44.4 87% 63%)',
+        },
       },
     },
   },
