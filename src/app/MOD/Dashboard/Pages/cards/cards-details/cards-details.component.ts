3966888010fbf419cdb6893ccba30efbf59b9f3a
iska1234 <sadhuisaac1@gmail.com>
2024-03-21 23:49:55 -0500

    Cards page 70%

---
+++ src/app/MOD/Dashboard/Pages/cards/cards-details/cards-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-details',
+  templateUrl: './cards-details.component.html',
+
+})
+export class CardsDetailsComponent   {
+
+
+}
