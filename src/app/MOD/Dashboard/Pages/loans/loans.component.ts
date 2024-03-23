f92e2f1d74d94277a1dc0c36b273657e59a980d9
Carlos Ochoa <uligibson@hotmail.com>
2024-03-22 20:22:41 -0600

    add fetched loans log

--- src/app/MOD/Dashboard/Pages/loans/loans.component.ts
+++ src/app/MOD/Dashboard/Pages/loans/loans.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoanService } from './service/loan.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
@@ -9,8 +9,18 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [TranslateModule, CommonModule],
 })
-export default class LoansComponent {
-  loans$ = this.loanService.getLoans('api/v1/loans');
+export default class LoansComponent implements OnInit {
+  loans$ = this.loanService.getLoans('api/v1/loans'); // This returns Observable<ILoanData[]>
+  ngOnInit(): void {
+    this.loans$.subscribe({
+      next: loans => {
+        console.log('loans fetched');
+        console.log(loans);
+      },
+      error: error => console.error('Error fetching loans:', error),
+      complete: () => console.log('Loan fetch operation completed'),
+    });
+  }
 
   constructor(private loanService: LoanService) {}
 }
